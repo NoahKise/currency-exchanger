fb9fd416afb31e55a7100dccd00444bf9b111845
noahkise <noah@kisefamily.com>
2023-11-17 12:28:38 -0800

    add detailed error handling

--- src/js/function.js
+++ src/js/function.js
@@ -4,10 +4,23 @@ export async function convert(cur1, cur2, amt) {
 
   try {
     const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Request failed with status: ' + response.status);
+    }
+
     const resultText = await response.text();
     const result = JSON.parse(resultText);
     return result;
   } catch (error) {
-    return error;
+    const response = await fetch(url);
+    const resultText = await response.text();
+    const result = JSON.parse(resultText);
+    const detailedError = {
+      message: 'There was an error in your API request',
+      originalError: error.message,
+      typeError: result["error-type"]
+    };
+    return detailedError;
   }
 }
