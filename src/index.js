84af86ba24d5f5310125bf4bca7e6474dab94aff
noahkise <noah@kisefamily.com>
2023-11-17 14:22:37 -0800

    remove useless names attributes from select forms, add correct result printing to DOM

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import { API } from "./js/function";
 
 async function handleFormSubmission(e) {
   e.preventDefault();
-  const cur1 = document.getElementById("cur1").value;
-  const cur2 = document.getElementById("cur2").value;
+  const cur1Element = document.getElementById("cur1");
+  const cur1 = cur1Element.value;
+  const cur1Name = cur1Element.options[cur1Element.selectedIndex].text;
+  const cur2Element = document.getElementById("cur2");
+  const cur2 = cur2Element.value;
+  const cur2Name = cur2Element.options[cur2Element.selectedIndex].text;
   const amt = document.getElementById("amount").value;
   const { result, error } = await API.convert(cur1, cur2, amt);
   if (error) {
@@ -18,6 +22,10 @@ async function handleFormSubmission(e) {
     console.log(detailedError);
   } else {
     console.log(result);
+    const conversion = document.createElement("p");
+    conversion.append(amt + " " + cur1Name + " is equal to " + result.conversion_result + " " + cur2Name + ".");
+    const body = document.querySelector("body");
+    body.append(conversion);
   }
 }
 
