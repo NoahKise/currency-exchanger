3ff1e2674f049d6574dd8847daf0ae2c2f596805
noahkise <noah@kisefamily.com>
2023-11-17 11:51:09 -0800

    add successful UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import { convert } from "./js/function";
+
+
+async function handleFormSubmission(e) {
+  e.preventDefault();
+  const cur1 = document.getElementById("cur1").value;
+  const cur2 = document.getElementById("cur2").value;
+  const amt = document.getElementById("amount").value;
+  const conversion = await convert(cur1, cur2, amt);
+  console.log(conversion);
+}
+
+
+document.getElementById("converter").addEventListener("submit", handleFormSubmission);
--- src/js/function.js
+++ src/js/function.js
@@ -1,13 +1,13 @@
 export async function convert(cur1, cur2, amt) {
   const apiKey = process.env.API_KEY;
-  const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${cur1}/${cur2}/${amt}`
+  const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${cur1}/${cur2}/${amt}`;
 
   try {
     const response = await fetch(url);
     const resultText = await response.text();
     const result = JSON.parse(resultText);
     return result;
-} catch (error) {
+  } catch (error) {
     return error;
-}
+  }
 }
