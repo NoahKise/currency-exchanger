c7e1c621860a66ec1eea1abe43c296969d69f21f
noahkise <noah@kisefamily.com>
2023-11-17 13:24:26 -0800

    get error handling working with console log

--- src/js/function.js
+++ src/js/function.js
@@ -15,7 +15,7 @@ export class API {
 
   static async convert(cur1, cur2, amt) {
     const apiKey = process.env.API_KEY;
-    const url = `https://v6.exchangerate-api.com/v6/${apiKey}//${cur1}/${cur2}/${amt}`;
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${cur1}/${cur2}/${amt}`;
     
     return await this.makeAPICall(url);
   }
